d520fee43a451622f106e36afd777b6f213c6acd
Maisie Bonnett <maisie.r.bonnett@gmail.com>
2026-09-25 12:11:47 -0700

    mongodb

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,38 +1,11 @@
-// backend.js
-import cors from "cors";
 import express from "express";
+import cors from "cors";
+
+import userServices from "./models/user-services.js";
 
 const app = express();
 const port = 8000;
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
+
 app.use(cors());
 app.use(express.json());
 
@@ -40,59 +13,52 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
+app.get("/users", async (req, res) => {
+  const name = req.query["name"];
+  const job = req.query["job"];
+  try {
+    const result = await userServices.getUsers(name, job);
+    res.send({ users_list: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
   }
 });
 
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const generateId = () => {
-  return Math.floor(Math.random() * 1000000).toString();
-};
-
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  userToAdd.id = generateId();
-  addUser(userToAdd);
-  res.status(201).send(userToAdd);
+app.get("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await userServices.findUserById(id);
+  if (result === undefined || result === null)
+    res.status(404).send("Resource not found.");
+  else {
+    res.send({ users_list: result });
+  }
 });
 
-app.delete("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const index = users["users_list"].findIndex(
-  (user) => user["id"] === id
-  );
-  if (index === -1) {
-    res.status(404).send("Resource not found.");
-  } else {
-    users["users_list"].splice(index, 1);
-    res.status(204).send();
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const savedUser = await userServices.addUser(user);
+  if (savedUser) res.status(201).send(savedUser);
+  else res.status(500).end();
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+
+  try {
+    const result = await userServices.deleteUserById(id);
+
+    if (result === null) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.status(204).send();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred in the server.");
   }
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
